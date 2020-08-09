d8e112b2a204cea9f5fffb733bf42a4370baf0dc
Eindray <eindray.aye@g.austincc.edu>
2020-08-08 21:45:32 -0500

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,31 @@
+/* Creating reverse triangles with the stars
+******** 
+****** 
+***** 
+**** 
+*** 
+** 
+* 
+*    */
+
+public class Triangle {
+
+	public static void main(String [] args){
+	
+	for (int i = 0; i < 10; i++){
+	
+		/*for(int j = 0; j < i ; j++){
+		System.out.print(" ");		
+		}*/
+		
+		for (int k=0; k<10-i; k++){
+			System.out.print("*");
+		}
+		
+		System.out.println(" ");
+		
+		}
+		
+	
+	}
+}
